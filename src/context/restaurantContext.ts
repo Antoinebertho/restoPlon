1dc3f4e0c69cbf638a47dcd8bc867a9a4da70c0c
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-03-17 14:18:26 +0100

    ajout contexte et model

--- src/context/restaurantContext.ts
+++ src/context/restaurantContext.ts
@@ -1,18 +1,5 @@
 import { createContext } from "react";
-
-type RestaurantType = {
-  id: number;
-  name: string;
-  address: string;
-  description_short: string;
-  description_long: string;
-  img: string;
-  menu: {
-    entrees: string[];
-    dishes: string[];
-    deserts: string[];
-  };
-};
+import { RestaurantType } from "../models/restaurantType";
 
 export type RestaurantContextType = {
   restaurants: RestaurantType[];
