1012a0302099f9392bd016d1223fec804ba543dc
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-03-18 23:30:35 +0100

    ajout img

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -15,36 +15,30 @@ const Section: React.FC<SectionProps> = () => {
         <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           {restaurants.map((restaurant) => (
             <a
+            href="/"
               key={restaurant.id}
               className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-              href="/"
             >
               <img
                 src={restaurant.img}
                 alt={restaurant.name}
-                className="h-32 w-32 mx-auto rounded-xl"
+                className="h-56 w-full rounded-md object-cover"
               />
               <h2 className="mt-4 text-xl font-bold text-white">
                 {restaurant.name}
               </h2>
-              <p className="mt-1 text-sm text-gray-300">{restaurant.address}</p>
-              <p className="mt-1 text-sm text-gray-300">
+              <p className="mt-1 text-xs text-gray-300 ">
                 {restaurant.description_short}
               </p>
+              <p className="text-xs mt-1.5 sm:mt-0 text-gray-500">{restaurant.address}</p>
               <div className="mt-12 text-center">
-                <a
-                  href="/"
-                  className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400  flex-col-reverse"
-                >
+                {/* <button className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400  flex-col-reverse">
                   See
-                </a>
-                <> </>
-                <a
-                  href="/"
-                  className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400  flex-col-reverse"
-                >
+                </button>
+                <> </> */}
+                <button className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400  flex-col-reverse">
                   Add
-                </a>
+                </button>
               </div>
             </a>
           ))}
--- src/data/datas.ts
+++ src/data/datas.ts
@@ -21,7 +21,7 @@ export const restaurants = [
       description_short: "Familial et végétarien",
       description_long:
         "Pour les amoureux des légumes et des produits de saison, Le Bain est un endroit idéal. Le tout dans un lieu agréable et cosy.",
-      img: "../../public/assets/Le bain.jpg"
+      img: "/assets/Le bain.jpg"
       /*"https://www.google.com/url?sa=i&url=https%3A%2F%2Fvisitbath.co.uk%2Ffood-and-drink%2Frestaurants&psig=AOvVaw08Wfzs5hb3qcBWRgVTZNFu&ust=1676632167576000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJD28dfzmf0CFQAAAAAdAAAAABAJ"*/,
       menu: {
         entrees: ["entree 1", "entree 2"],
@@ -30,21 +30,20 @@ export const restaurants = [
       },
     },
     {
-      id: 2,
-      name: "Aux filets bleus",
-      address:
-        "67 route du Port de Locquémeau Pointe de Sehar, 22300 Trédrez-Locquémeau",
-      description_short: "Restaurant de poissons",
-      description_long:
-        "Aux filets bleus vous accueille pour venir déguster la pêche du jour ! Dans un cadre magnifique avec vue directe sur la mer.",
-      img: "../../public/assets/Les filets bleue.png"
-      /*"https://www.google.com/maps/uv?pb=!1s0x4813d46038dbc27d:0x41469dae0cd46ea8!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipPWYcBjbpnej2FQoHGYsqQmCY1NOA7v5n-0L49z%3Dw520-h350-n-k-no!5sfilets+bleus+-+Recherche+Google!15zQ2dJZ0FRPT0&imagekey=!1e10!2sAF1QipPWYcBjbpnej2FQoHGYsqQmCY1NOA7v5n-0L49z&hl=fr&sa=X&ved=2ahUKEwjtvb3--pn9AhX2WqQEHVWjAi4Q7ZgBKAB6BAgQEAI"*/,
-      menu: {
-        entrees: ["entree 1", "entree 2"],
-        dishes: ["plat 1", "plat 2", "plat 3"],
-        deserts: ["desert 1", "desert 2"],
+        id: 2,
+        name: "Higuma",
+        address: "32bis Rue Sainte-Anne, 75001 Paris",
+        description_short: "Cuisine japonaise, cadre à l'ambiance détendue",
+        description_long:
+          "Restaurant japonais de style décontracté servant ramens, plats sautés et donburis, accompagnés de bière et de saké.",
+        img:  "/assets/higuma.png"
+        /*"https://www.google.com/maps/uv?pb=!1s0x47e66e3acb056675:0xfe3f9961811e978c!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipOgTJjdnpMoYT6XXw1LbLRmfNZqmGqRPUsWYDtx%3Dw520-h350-n-k-no!5shiguma+paris+-+Recherche+Google!15zQ2dJZ0FRPT0&imagekey=!1e10!2sAF1QipOgTJjdnpMoYT6XXw1LbLRmfNZqmGqRPUsWYDtx&hl=fr&sa=X&ved=2ahUKEwj34bLi-5n9AhV9TKQEHUP0DrgQ7ZgBKAB6BAgXEAI"*/,
+        menu: {
+          entrees: ["entree 1", "entree 2"],
+          dishes: ["plat 1", "plat 2", "plat 3"],
+          deserts: ["desert 1", "desert 2"],
+        },
       },
-    },
     {
       id: 3,
       name: "Le Balto",
@@ -52,7 +51,7 @@ export const restaurants = [
       description_short: "Brasserie parisienne",
       description_long:
         "Soupe à l'oignon, charcuterie et steaks servis dans une brasserie haut de gamme avec des salles décorées dans des styles différents.",
-      img: "../../public/assets/le balto.png"
+      img: "/assets/le balto.png"
       /*"https://www.google.com/maps/uv?pb=!1s0x47e671d8b9183cef:0x999004ac679e6812!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipPLd5qp49GeRegeQVn4k65QcVF0UBSl5YMCKVDY%3Dw520-h350-n-k-no!5sme+balto+-+Recherche+Google!15zQ2dJZ0FRPT0&imagekey=!1e10!2sAF1QipPLd5qp49GeRegeQVn4k65QcVF0UBSl5YMCKVDY&hl=fr&sa=X&ved=2ahUKEwiRtump-5n9AhXEVaQEHdKxDzMQ7ZgBKAB6BAgUEAI"*/,
       menu: {
         entrees: ["entree 1", "entree 2"],
@@ -67,7 +66,7 @@ export const restaurants = [
       description_short: "Cuisine basque et chaleureuse",
       description_long:
         "Plats basques gastronomiques dans un lieu aux tons chauds et colorés, avec chaises et comptoir en bois.",
-      img: "../../public/assets/au bascout.png"
+      img: "/assets/au bascout.png"
       /*"https://www.google.com/maps/uv?pb=!1s0x47e66e0544d1174f:0xf21ae3437e56ebb!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipOdlk8YKbDWmQDZIwkKagRpwDtEEmK_20FhP0mv%3Dw520-h350-n-k-no!5srestaurants+-+Recherche+Google!15zQ2dJZ0FRPT0&imagekey=!1e10!2sAF1QipOdlk8YKbDWmQDZIwkKagRpwDtEEmK_20FhP0mv&hl=fr&sa=X&ved=2ahUKEwju0sPp-pn9AhUfTKQEHdBqApkQ7ZgBKAB6BAgHEAI"*/,
       menu: {
         entrees: ["entree 1", "entree 2"],
@@ -82,7 +81,7 @@ export const restaurants = [
       description_short: "Brasserie pittoresque de spécialités françaises",
       description_long:
         "Cuisine et boissons de brasserie parisienne classique dans un restaurant établi de longue date avec terrasse au bord du canal.",
-      img: "../../public/assets/le bastringue.png"
+      img: "/assets/le bastringue.png"
       /*"https://www.google.com/maps/uv?pb=!1s0x47e66dd0e3eb82df:0x9067c10bed8a820b!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipOTX7AS7zZ5qVJWERvy8rru_MA22MXDv5pTAcpE%3Dw520-h350-n-k-no!5sle+bastringue+paris+19+-+Recherche+Google!15zQ2dJZ0FRPT0&imagekey=!1e10!2sAF1QipOTX7AS7zZ5qVJWERvy8rru_MA22MXDv5pTAcpE&hl=fr&sa=X&ved=2ahUKEwj6gK_J-5n9AhX9TKQEHXkjBxkQ7ZgBKAB6BAgSEAI"*/,
       menu: {
         entrees: ["entree 1", "entree 2"],
@@ -91,18 +90,19 @@ export const restaurants = [
       },
     },
     {
-      id: 6,
-      name: "Higuma",
-      address: "32bis Rue Sainte-Anne, 75001 Paris",
-      description_short: "Cuisine japonaise, cadre à l'ambiance détendue",
-      description_long:
-        "Restaurant japonais de style décontracté servant ramens, plats sautés et donburis, accompagnés de bière et de saké.",
-      img:  "../../public/assets/higuma.png"
-      /*"https://www.google.com/maps/uv?pb=!1s0x47e66e3acb056675:0xfe3f9961811e978c!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipOgTJjdnpMoYT6XXw1LbLRmfNZqmGqRPUsWYDtx%3Dw520-h350-n-k-no!5shiguma+paris+-+Recherche+Google!15zQ2dJZ0FRPT0&imagekey=!1e10!2sAF1QipOgTJjdnpMoYT6XXw1LbLRmfNZqmGqRPUsWYDtx&hl=fr&sa=X&ved=2ahUKEwj34bLi-5n9AhV9TKQEHUP0DrgQ7ZgBKAB6BAgXEAI"*/,
-      menu: {
-        entrees: ["entree 1", "entree 2"],
-        dishes: ["plat 1", "plat 2", "plat 3"],
-        deserts: ["desert 1", "desert 2"],
+        id: 6,
+        name: "Aux filets bleus",
+        address:
+          "67 route du Port de Locquémeau Pointe de Sehar, 22300 Trédrez-Locquémeau",
+        description_short: "Restaurant de poissons",
+        description_long:
+          "Aux filets bleus vous accueille pour venir déguster la pêche du jour ! Dans un cadre magnifique avec vue directe sur la mer.",
+        img: "/assets/Les filets bleue.png"
+        /*"https://www.google.com/maps/uv?pb=!1s0x4813d46038dbc27d:0x41469dae0cd46ea8!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipPWYcBjbpnej2FQoHGYsqQmCY1NOA7v5n-0L49z%3Dw520-h350-n-k-no!5sfilets+bleus+-+Recherche+Google!15zQ2dJZ0FRPT0&imagekey=!1e10!2sAF1QipPWYcBjbpnej2FQoHGYsqQmCY1NOA7v5n-0L49z&hl=fr&sa=X&ved=2ahUKEwjtvb3--pn9AhX2WqQEHVWjAi4Q7ZgBKAB6BAgQEAI"*/,
+        menu: {
+          entrees: ["entree 1", "entree 2"],
+          dishes: ["plat 1", "plat 2", "plat 3"],
+          deserts: ["desert 1", "desert 2"],
+        },
       },
-    },
   ];
