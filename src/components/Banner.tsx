709323ce343a19396df1fa8c69acb5c290e5621d
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-03-19 02:31:30 +0100

    ajustements

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -20,7 +20,7 @@ const Banner: React.FC<BannerProps> = () => {
             <button
               className="inline-block rounded bg-gray-900 px-8 py-3 text-sm font-medium text-white transition hover:rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-indigo-500 hover:bg-indigo-500 "
             >
-              Favorites ♥️
+              Favorites 💜
             </button>
           </div>
         </div>
