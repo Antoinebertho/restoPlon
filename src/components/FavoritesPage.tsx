0c54665fb1bef7766613738011596825855030cf
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-03-31 17:29:47 +0200

    ajout favoris

--- src/components/FavoritesPage.tsx
+++ src/components/FavoritesPage.tsx
@@ -24,6 +24,9 @@ const FavoritesPage = () => {
     return favorites.includes(restaurantId);
   };
 
+  const favoriteRestaurants = restaurants.filter((restaurant)=>
+    isFavorite(restaurant.id))
+
   return (
     <section className="bg-gray-900 text-white">
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
@@ -31,7 +34,7 @@ const FavoritesPage = () => {
           <h2 className="text-4xl font-bold sm:text-4xl">Mes favoris</h2>
         </div>
         <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {restaurants.map((restaurant) => (
+          {favoriteRestaurants.map((restaurant) => (
             <div
               key={restaurant.id}
               className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
