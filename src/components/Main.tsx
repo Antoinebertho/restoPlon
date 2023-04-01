b76f833e303dd580aff06437dd65181d2d4b8dd3
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-04-01 14:00:36 +0200

    changement nom bouton

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -55,7 +55,7 @@ const Main = () => {
                   className="inline-block rounded bg-pink-600 px-8 py-3 text-sm font-medium text-white transition hover:-rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-pink-900 focus:ring-yellow-400 hover:bg-pink-700 mr-6"
                   onClick={() => handleCheckClick(restaurant)}
                 >
-                  Voir +
+                  Découvrir
                 </button>
                 <> </>
                 {isFavorite(restaurant.id) ? (
