38e9358f317280f4aa319c25af5bbc69c80babdb
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-04-07 11:34:41 +0200

    changement type export

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Banner from "./components/Banner";
-import Footer from "./components/Footer";
-import Main from "./pages/MainPage";
+import {Banner} from "./components/Banner";
+import {Footer} from "./components/Footer";
+import {MainPage} from "./pages/MainPage";
 import { RestaurantContext } from "./context/restaurantContext";
 import { restaurantsData } from "./data/restaurantsDatas";
 import { FavoritesProvider } from "./context/FavoritesContext";
-import FavoritesPage from "./pages/FavoritesPage";
+import {FavoritesPage} from "./pages/FavoritesPage";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <FavoritesProvider>
           <RestaurantContext.Provider value={{ restaurants: restaurantsData }}>
             <Routes>
-              <Route path="/" element={<Main />} />
+              <Route path="/" element={<MainPage />} />
               <Route path="/FavoritesPage" element={<FavoritesPage />} />
             </Routes>
           </RestaurantContext.Provider>
